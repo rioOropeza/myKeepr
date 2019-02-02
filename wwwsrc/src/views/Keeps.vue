e2476f94d5dbc01ebcaa9cb9118da6d9e3bead17
rioOropeza <blasphemy678@live.com>
2019-02-02 12:59:01 -0700

    commit

--- wwwsrc/src/views/Keeps.vue
+++ wwwsrc/src/views/Keeps.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="container-fluid">
       <div class="row">
-        <div class="col-8">
+        <div class="col-12">
           <h1>Keeps</h1>
         </div>
       </div>
